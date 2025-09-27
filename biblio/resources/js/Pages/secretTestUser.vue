b258194b7090d067b8de26bd7cf0920e85667ee7
Kristiana Liepa <kl00404@rvt.lv>
2025-09-27 20:27:39 +0300

    kkads pagajusa gada commit

---
+++ biblio/resources/js/Pages/secretTestUser.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="flex items-center justify-center h-screen bg-gray-100">
+        <h1 class="text-5xl font-bold">Bonjour, Daiga!</h1>
+    </div>
+</template>
+
+<script setup>
+// Nav vajadzīgs nekāds papildu JS šeit
+</script>
+
